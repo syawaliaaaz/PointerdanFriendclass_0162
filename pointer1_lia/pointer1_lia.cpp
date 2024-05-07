88dd848a863d70a987649505ec59482b214f7681
syawaliaaaz <helfinasyawaliaaa@gmail.com>
2024-05-07 12:10:42 +0700

    Integer Main class mahasiswa

--- pointer1_lia/pointer1_lia.cpp
+++ pointer1_lia/pointer1_lia.cpp
@@ -10,6 +10,23 @@ public:
         cout << "No induk = " << nim << endl;
     }
 
+    int main() {
+
+        mahasiswa mhs{ 1 }; //Object mhs
+        mhs.showNim(); //Member access operator
+
+        mahasiswa& refMhs = mhs; //Pointer reference refMhs
+        refMhs.nim = 1; //Member access operator
+        mhs.showNim();
+
+        mahasiswa* pMhs = &mhs; //Pointer reference pMhs
+        pMhs->nim = 3; //Access operator
+        pMhs->showNim();
+        return 0;
+    }
+};
+
+
 
 
 
