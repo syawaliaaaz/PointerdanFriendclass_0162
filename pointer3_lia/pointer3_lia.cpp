780ed0ca2162547d24047ba23cd5577213374727
syawaliaaaz <helfinasyawaliaaa@gmail.com>
2024-05-07 12:17:24 +0700

    Input private hingga void siswa

--- pointer3_lia/pointer3_lia.cpp
+++ pointer3_lia/pointer3_lia.cpp
@@ -7,5 +7,27 @@ class siswa;
 class orang
 {
 
+
+private:
+    string nama;
+
+public:
+    void setNama(string pNama);
+    friend class siswa;
 };
+class siswa
+{
+private:
+    int id;
+
+public:
+    void setId(int pId);
+    void displayAll(orang& a);
+};
+
+void siswa::displayAll(orang& a)
+{
+    cout << id << endl << a.nama;
+}
+
 
